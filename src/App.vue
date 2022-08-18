f7147367786c7494b862844d1e840d4722f38106
fahadzardari <fahadzardari111@gmail.com>
2022-08-18 10:19:12 +0500

    preliminary design for home created

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,72 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
-
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="body px-4 pt-2 md:px-16 md:pt-6 bg-[#2B2525] min-h-screen">
+    <div class="flex flex-row container justify-between text-white ">
+      <div class="text-yellow-400 ">CRYPTOHUNTER</div>
+      <div>
+        <select name="" id="" class="bg-[#2B2525]  rounded-md p-1">
+          <optgroup class="bg-[#2B2525] ">
+            <option value="">USD</option>
+            <option value="">PKR</option>
+          </optgroup>
+        </select>
+      </div>
+    </div>
+
+
+    <div class="title bg-blue-300 text-center text-white w-full h-full flex flex-col justify-center items-center">
+      <h1 class="text-2xl">CRYPTOHUNTER</h1>
+      <h3>Get all the info regarding your favorite Cryptocurrency</h3>
+    </div>
+
+    <div class="home mt-[2rem]">
+      <section class="text-center text-white">
+        <h1>Cryptocurrency Prices by Market Cap</h1>
+      </section>
+      <div class="container flex justify-center items-center w-full mt-[1rem]">
+        <div class="w-full flex flex-col items-center justify-center">
+
+          <input type="text" class="w-2/3 border border-white rounded-md bg-transparent text-white p-1"
+            placeholder="Search for a Cryptocurrency...">
+          <table class="list mt-2 w-2/3 ">
+              <thead class="">
+                  <tr class="bg-yellow-500 ">
+                      <th class="px-[2rem] rounded-l-md">Coin</th>
+                      <th class="px-[2rem] ">Price</th>
+                      <th class="px-[2rem] ">24h change</th>
+                      <th class="px-[2rem] rounded-r-md">Market Cap</th>
+                  </tr>
+              </thead>
+              <tbody class="text-white text-center">
+                  <tr>
+                    <td class="px-[2rem]">dummy</td>
+                    <td class="px-[2rem]">dummy</td>
+                    <td class="px-[2rem]">dummy</td>
+                    <td class="px-[2rem]">dummy</td>
+                  </tr>
+              </tbody>
+          </table>
+
+        </div>
+      </div>
+
+    </div>
+
   </div>
-  <HelloWorld msg="Vite + Vue" />
+
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
+<script>
+import Home from "./components/Home.vue"
+export default {
+  components: {
+    Home,
+  },
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+</script>
+
+<style>
+select:focus {
+  box-shadow: none;
+  outline: none;
 }
 </style>
