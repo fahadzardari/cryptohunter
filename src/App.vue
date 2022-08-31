2dfa75e4aafb550adb1f23b00c2500c3a416a8a3
fahadzardari <fahadzardari111@gmail.com>
2022-08-31 22:52:08 +0500

    graph gradient added + minor changes

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,6 @@
   <head>
     <link rel="icon" href="./assets/logo3.png" type="image/png">
   </head>
-  {{ currencies[77] }}
   <div class="body px-4 pt-2 md:px-16 md:pt-6 bg-[#2B2525] min-h-screen">
     <div v-show="!detailsScreen && !newsScreen">
       <div class="flex flex-row container justify-between text-white ">
@@ -15,7 +14,7 @@
         <div>
           <select v-model="currencyId" @change="changeCurrency()" class="bg-[#2B2525]  rounded-md p-1">
             <optgroup class="bg-[#2B2525]">
-              <option v-for="(currency, index) in currencies" :key="index" :value="index">{{ currency.abbreviation }}
+              <option v-for="(currency, index) in currencies" :key="index" :value="index">{{  currency.abbreviation  }}
               </option>
             </optgroup>
           </select>
@@ -29,12 +28,16 @@
         <div class="grid grid-cols-4 flex-row justify-between gap-4">
           <div v-for="n in 4" :key="n" class="card rounded-md flex flex-col p-2 items-center ">
             <img :src="list[n - 1].image" alt="{{list[n-1].id}}" class="h-[2rem] w-[2rem]">
-            <h1>{{ list[n - 1].name }} <span
-                :class="(list[n - 1].price_change_24h < 0) ? 'text-red-500' : 'text-green-500'">{{ list[n -
-                    1].price_change_percentage_24h.toFixed(4)
-                    * 10
+            <h1>{{  list[n - 1].name  }} <span
+                :class="(list[n - 1].price_change_24h < 0) ? 'text-red-500' : 'text-green-500'">{{  list[n -
+                1].price_change_percentage_24h.toFixed(4)
+                * 10
+
+
+
+
                 }}%</span></h1>
-            <h1>{{ currencies[currencyId]['symbol'] }}{{ list[n - 1].current_price }}</h1>
+            <h1>{{  currencies[currencyId]['symbol']  }}{{  list[n - 1].current_price  }}</h1>
           </div>
         </div>
       </div>
@@ -59,15 +62,15 @@
                 <tr v-for="(crypto, index) in listToShow" :key="index" @click="goToDetails(crypto.id, 1, 'hourly')"
                   class="cursor-pointer ">
                   <td class="px-[2rem] flex items-center font-bold"><img :src="crypto.image"
-                      class="w-[1rem] h-[1rem] mr-[0.1rem]" alt="">{{ crypto.name }}</td>
-                  <td class="px-[2rem]">{{ currencies[currencyId]['symbol'] }} {{ crypto.current_price.toFixed(3) }}
+                      class="w-[1rem] h-[1rem] mr-[0.1rem]" alt="">{{  crypto.name  }}</td>
+                  <td class="px-[2rem]">{{  currencies[currencyId]['symbol']  }} {{  crypto.current_price.toFixed(3)  }}
                   </td>
                   <td class="px-[2rem]" :class="(crypto.price_change_24h < 0) ? 'text-red-500' : 'text-green-500'">
                     <span :class="(crypto.price_change_24h > 0) ? '' : 'hidden'">+</span>{{
-                        (crypto.price_change_percentage_24h).toFixed(3)
+                     (crypto.price_change_percentage_24h).toFixed(3) 
                     }} %
                   </td>
-                  <td class="px-[2rem]">{{ currencies[currencyId]['symbol'] }} {{ crypto.market_cap }}</td>
+                  <td class="px-[2rem]">{{  currencies[currencyId]['symbol']  }} {{  crypto.market_cap  }}</td>
                 </tr>
               </tbody>
             </table>
@@ -103,16 +106,16 @@
       <div class="detailsScreen mt-[3rem] md:grid md:grid-cols-8">
         <div class="md:col-span-2  flex flex-col gap-4 items-center justify-center text-white  details">
           <img :src="currentCoinDetail.image['large']" class="h-[8rem] w-[8rem]">
-          <p class="text-[1.8rem] uppercase">{{ currentCoinDetail.name }}</p>
-          <p class="text-[1.2rem] uppercase">Rank:{{ currentCoinDetail.market_cap_rank }}</p>
-          <p class="text-[1.2rem] uppercase">Price:{{ currencies[currencyId]['symbol'] }} </p>
+          <p class="text-[1.8rem] uppercase">{{  currentCoinDetail.name  }}</p>
+          <p class="text-[1.2rem] uppercase">Rank:{{  currentCoinDetail.market_cap_rank  }}</p>
+          <p class="text-[1.2rem] uppercase">Price:{{  currencies[currencyId]['symbol']  }} </p>
           <p class="text-[1.2rem] uppercase">24h Change:<span class="text-green-700">1%</span></p>
           <p class="text-[1.2rem] uppercase">All-Time-High:$299999</p>
           <p class="text-[1.2rem] uppercase">All-Time-Low:$299999</p>
           <p class="text-[1.2rem] uppercase">Market-cap:88889999</p>
         </div>
         <div class="md:col-span-6  graph">
-          <canvas id="myChart" class="bg-white"></canvas>
+          <canvas id="myChart" class=""></canvas>
         </div>
       </div>
 
@@ -136,7 +139,7 @@
             labore.
           </p>
         </div>
-                <div href="https://somlink.com" class="bg-white">
+        <div href="https://somlink.com" class="bg-white">
           <img src="./assets/bg.jpg" class="">
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse voluptatum a nulla quasi? Fugit repellendus
             sit doloribus modi natus voluptatem odit accusantium, id molestiae in, consequatur nobis iusto itaque
@@ -180,7 +183,9 @@ export default {
 
   methods: {
     async getFullList() {
-      this.currencyId = localStorage.getItem("currencyId") || 77;
+      // this.currencyId =  4;
+     this.currencyId = localStorage.getItem("currencyId") || 77;
+      
       this.currencyToShowIn = localStorage.getItem("currencyToShowIn") || "pkr";
       let response = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=' + this.currencyToShowIn + '&order=market_cap_desc');
       return response.json();
@@ -222,7 +227,7 @@ export default {
           console.log(data['prices'][0][1].toFixed(2))
           for (var i = 0; i < data['prices'].length; i++) {
             pricesToShow.push(data['prices'][i][1].toFixed(2));
-            labelsToShow.push(data['prices'][i][0].toFixed(2));
+            labelsToShow.push(" ");
 
           }
           console.log(pricesToShow);
@@ -238,7 +243,22 @@ export default {
           if (chartStatus != undefined) {
             chartStatus.destroy();
           }
-          const ctx = document.getElementById('myChart');
+          const colors = {
+            purple: {
+              default: "rgba(234 , 179 , 8 , 1)",
+              half: "rgba(234 , 179 , 8, 0.75)",
+              quarter: "rgba(234 , 179 , 8, 0.25)",
+               zero: "rgba(234 , 179 , 8, 0.45)"
+            }
+            
+          };
+
+          const ctx = document.getElementById('myChart').getContext('2d');
+
+          const gradient = ctx.createLinearGradient(0, 25, 0, 300);
+          gradient.addColorStop(0, colors.purple.default);
+          gradient.addColorStop(0.55, colors.purple.half);
+          gradient.addColorStop(1, colors.purple.zero);
           const myChart = new Chart(ctx, {
             type: 'line',
             data: {
@@ -248,10 +268,11 @@ export default {
                 data: pricesToShow,
                 borderWidth: 2,
                 borderColor: 'black',
-                backgroundColor: ['#9AB8F7', '#9A3BC7'],
-                fill: 'start',
+                backgroundColor: gradient,
+                fill: true,
                 tension: 0.5,
-                pointRadius: 1
+                pointRadius: 1,
+                grid: false
               }]
             },
             options: {
@@ -259,7 +280,18 @@ export default {
                 y: {
                   beginAtZero: false,
                   suggestedMin: Math.min(...pricesToShow) - Math.min(...pricesToShow) / 10,
-                  suggestedMax: Math.max(...pricesToShow) + Math.max(...pricesToShow) / 10
+                  suggestedMax: Math.max(...pricesToShow) + Math.max(...pricesToShow) / 10,
+
+                  grid: {
+                    display: false
+                  }
+
+                },
+                x: {
+                  display: false,
+                  grid: {
+                    display: false
+                  }
                 }
               }
             }
